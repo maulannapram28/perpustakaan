35d066fd38ba7849502458d9aeade4ad69f58dcf
maulannapram28 <maulannapram28@gmail.com>
2018-07-11 01:06:12 +0700

    menambah layout

---
+++ application/views/layout/header.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="<?php echo base_url('admin/dasbor') ?>">SI Perpus</a> 
+            </div>
+  <div style="color: white;
+padding: 15px 50px 5px 50px;
+float: right;
+font-size: 16px;"> <?php echo date ('d M Y') ?> &nbsp; 
+
+<a href="<?php echo base_url('admin/dasbor/profile') ?>" class="btn btn-success square-btn-adjust"><i class="fa fa-user"></i> user login </a> 
+
+<a href="<?php echo base_url() ?>" class="btn btn-primary square-btn-adjust" target="_blank"><i class="fa fa-home"></i> homepage </a> 
+<a href="login.html" class="btn btn-danger square-btn-adjust">Logout</a> 
+
+</div>
+        </nav>   
+           <!-- /. NAV TOP  -->
---
+++ application/views/layout/nav.php
@@ -0,0 +1,46 @@
+<nav class="navbar-default navbar-side" role="navigation">
+    <div class="sidebar-collapse">
+        <ul class="nav" id="main-menu">
+			
+            <li>
+                <a  href="<?php echo base_url('admin/dasbor') ?>"><i class="fa fa-dashboard"></i> Dashboard</a>
+            </li>
+           
+                
+            </li>	
+			                   
+            <li><a href="#"><i class="fa fa-user"></i> User/Admin<span class="fa arrow"></span></a>
+                <ul class="nav nav-second-level">
+                    <li><a href="<?php echo base_url('admin/dasbor') ?>">Data User</a></li>
+                     <li><a href="<?php echo base_url('admin/user/tambah') ?>">Tambah User</a></li>
+                        </ul>
+                    </li>
+                </ul>
+              </li>  
+        
+        </ul>
+       
+    </div>
+    
+</nav>  
+<!-- /. NAV SIDE  -->
+<div id="page-wrapper" >
+    <div id="page-inner">
+        <div class="row">
+            <div class="col-md-12">
+             <h2><?php echo $title ?></h2>   
+               
+            </div>
+        </div>
+         <!-- /. ROW  -->
+         <hr />
+       
+    <div class="row">
+        <div class="col-md-12">
+            <!-- Advanced Tables -->
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                     <?php echo $title ?>
+                </div>
+                <div class="panel-body">
+                    <div class="table-responsive">
